f2f8563b4711f9b9c0e37abcce5016d7e96fffc0
gedra <gedra@92628ce9-7369-4bad-8269-4a200b7df828>
2004-06-05 17:17:12 +0000

    Recevier status register

---
+++ rtl/vhdl/rx_status_reg.vhd
@@ -0,0 +1,68 @@
+----------------------------------------------------------------------
+----                                                              ----
+---- WISHBONE SPDIF IP Core                                       ----
+----                                                              ----
+---- This file is part of the SPDIF project                       ----
+---- http://www.opencores.org/cores/spdif_interface/              ----
+----                                                              ----
+---- Description                                                  ----
+---- SPDIF receiver status register                               ----
+----                                                              ----
+----                                                              ----
+---- To Do:                                                       ----
+---- -                                                            ----
+----                                                              ----
+---- Author(s):                                                   ----
+---- - Geir Drange, gedra@opencores.org                           ----
+----                                                              ----
+----------------------------------------------------------------------
+----                                                              ----
+---- Copyright (C) 2004 Authors and OPENCORES.ORG                 ----
+----                                                              ----
+---- This source file may be used and distributed without         ----
+---- restriction provided that this copyright statement is not    ----
+---- removed from the file and that any derivative work contains  ----
+---- the original copyright notice and the associated disclaimer. ----
+----                                                              ----
+---- This source file is free software; you can redistribute it   ----
+---- and/or modify it under the terms of the GNU Lesser General   ----
+---- Public License as published by the Free Software Foundation; ----
+---- either version 2.1 of the License, or (at your option) any   ----
+---- later version.                                               ----
+----                                                              ----
+---- This source is distributed in the hope that it will be       ----
+---- useful, but WITHOUT ANY WARRANTY; without even the implied   ----
+---- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      ----
+---- PURPOSE. See the GNU Lesser General Public License for more  ----
+---- details.                                                     ----
+----                                                              ----
+---- You should have received a copy of the GNU Lesser General    ----
+---- Public License along with this source; if not, download it   ----
+---- from http://www.opencores.org/lgpl.shtml                     ----
+----                                                              ----
+----------------------------------------------------------------------
+--
+-- CVS Revision History
+--
+-- $Log: not supported by cvs2svn $
+--
+
+library IEEE;
+use IEEE.std_logic_1164.all; 
+use IEEE.std_logic_arith.all;
+
+entity rx_status_reg is	 
+  generic (DATA_WIDTH: integer);
+  port (
+    status_rd: in std_logic;            -- status register read
+    status_vector: in std_logic_vector(DATA_WIDTH - 1 downto 0); 
+    status_dout: out std_logic_vector(DATA_WIDTH - 1 downto 0));
+end rx_status_reg;
+
+architecture rtl of rx_status_reg is
+
+begin
+	
+  status_dout <= status_vector when status_rd = '1' else (others => '0');
+	
+end rtl;
