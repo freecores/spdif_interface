6672dff33e23c131b38c87041252e4e251df4675
gedra <gedra@92628ce9-7369-4bad-8269-4a200b7df828>
2004-06-16 19:02:28 +0000

    Added frame decoder

--- bench/vhdl/tb_gen_spdif.vhd
+++ bench/vhdl/tb_gen_spdif.vhd
@@ -45,6 +45,9 @@
 -- CVS Revision History
 --
 -- $Log: not supported by cvs2svn $
+-- Revision 1.2  2004/06/13 18:10:20  gedra
+-- Renamed generic
+--
 -- Revision 1.1  2004/06/06 15:44:19  gedra
 -- Simple test bench for rx_phase_det.vhd.
 --
@@ -85,13 +88,49 @@ architecture behav of tb_gen_spdif is
     cs_b_en: out std_logic);              -- channel status ch. B enable);
   end component;
   
+  component rx_decode 
+    generic (DATA_WIDTH: integer range 16 to 32;
+             ADDR_WIDTH: integer range 8 to 64);   
+    port (
+      wb_clk_i: in std_logic;
+      conf_rxen: in std_logic;
+      conf_sample: in std_logic;
+      conf_valid: in std_logic;
+      conf_mode: in std_logic_vector(3 downto 0);
+      conf_blken: in std_logic;
+      conf_valen: in std_logic;
+      conf_useren: in std_logic;
+      conf_staten: in std_logic;
+      conf_paren: in std_logic;
+      lock: in std_logic;
+      rx_data: in std_logic;
+      rx_data_en: in std_logic;
+      rx_block_start: in std_logic;
+      rx_frame_start: in std_logic;
+      rx_channel_a: in std_logic; 
+      wr_en: out std_logic;
+      wr_addr: out std_logic_vector(ADDR_WIDTH - 2 downto 0);
+      wr_data: out std_logic_vector(DATA_WIDTH - 1 downto 0);
+      stat_paritya: out std_logic;
+      stat_parityb: out std_logic;
+      stat_lsbf: out std_logic;
+      stat_hsbf: out std_logic);
+  end component;
+  
   signal reset, spdif, rx_frame_start: std_logic;
   signal lock, rx_data, rx_data_en, rx_block_start : std_logic;
   signal rx_channel_a, wb_clk_i, rxen, rx_error : std_logic;
-  signal ud_a_en, ud_b_en, cs_a_en, cs_b_en : std_logic;
+  signal ud_a_en, ud_b_en, cs_a_en, cs_b_en, zero, one : std_logic;
+  signal stat_paritya, stat_parityb, stat_lsbf, stat_hsbf : std_logic;
+  signal wr_en : std_logic;
+  signal wr_addr: std_logic_vector(6 downto 0);
+  signal wr_data: std_logic_vector(31 downto 0);
   
 begin  
 
+  zero <= '0';
+  one <= '1';
+  
   -- Soruce generating SPDIF signal used to train the decoder
   GS: spdif_source
     generic map (
@@ -120,6 +159,36 @@ begin
       cs_a_en => cs_a_en,
       cs_b_en => cs_b_en);
 
+  -- frame decoder
+  FD: rx_decode
+    generic map (
+      DATA_WIDTH => 32,
+      ADDR_WIDTH => 8)
+    port map (
+      wb_clk_i => wb_clk_i,
+      conf_rxen => rxen,
+      conf_sample => rxen,
+      conf_valid => zero,
+      conf_mode => "0000",
+      conf_blken => one,
+      conf_valen => zero,
+      conf_useren => zero,
+      conf_staten => zero,
+      conf_paren => one,
+      lock => lock,
+      rx_data => rx_data,
+      rx_data_en => rx_data_en,
+      rx_block_start => rx_block_start,
+      rx_frame_start => rx_frame_start,
+      rx_channel_a => rx_channel_a,
+      wr_en => wr_en,
+      wr_addr => wr_addr,
+      wr_data => wr_data,
+      stat_paritya => stat_paritya,
+      stat_parityb => stat_parityb,
+      stat_lsbf => stat_lsbf,
+      stat_hsbf => stat_hsbf);
+      
   rxen <= not reset;
 
   -- just generate a reset signal, the rest is waveform studies...
@@ -141,3 +210,4 @@ begin
   end process;
   
 end behav;
+
